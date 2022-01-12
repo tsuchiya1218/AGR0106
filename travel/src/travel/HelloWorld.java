f362031021d3603bcd8c8d9f233f5e14f64663fb
20jy0124 <20jy0124@PC28.jynet.jec.ac.jp>
2022-01-12 14:20:56 +0900

    test

---
+++ travel/src/travel/HelloWorld.java
@@ -0,0 +1,12 @@
+package travel;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+	}
+
+}
